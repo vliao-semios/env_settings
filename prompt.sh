4e3f879b8ab898b1e25f998d3b7ba3b183eaea7e
VincentL <vincent@questflex.com>
2018-08-25 22:18:19 -0700

    fix command substitution

--- prompt.sh
+++ prompt.sh
@@ -8,51 +8,65 @@ function parse_git_branch() {
 	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 	if [ ! "${BRANCH}" == "" ]
 	then
-        REMOTE=`git config --get-regexp "branch\.${BRANCH}\.remote" | sed -e "s/^.* //"`
-        test ${REMOTE} && REMOTE_BRANCH=`git config --get-regexp "branch\.${BRANCH}\.merge" | sed -e "s/^.* //" -e "s/refs\/.*\///"`
-        test ${REMOTE_BRANCH} && REMOTE_STR=${REMOTE}/${REMOTE_BRANCH}
-
-		STAT=`parse_git_dirty`
-		echo "\[\e[32m\][${BRANCH}->\[\e[m\]\[\e[31m\]${REMOTE_STR}|\[\e[m\]\[\e[32m\]${STAT}]\[\e[m\]"
+		echo ${BRANCH}
 	else
 		echo ""
 	fi
+}
+
+function parse_git_remote_branch(){
+	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+	if [ ! "${BRANCH}" == "" ]
+	then
+        REMOTE=`git config --get-regexp "branch\.${BRANCH}\.remote" | sed -e "s/^.* //"`
+        test ${REMOTE} && REMOTE_BRANCH=`git config --get-regexp "branch\.${BRANCH}\.merge" | sed -e "s/^.* //" -e "s/refs\/.*\///"`
+        test ${REMOTE_BRANCH} && REMOTE_STR=${REMOTE}/${REMOTE_BRANCH}
+        echo ${REMOTE_STR}
+    else
+        echo ""
+    fi
+
 }
 
 # get current status of git repo
 function parse_git_dirty {
-	status=`git status 2>&1 | tee`
-	dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-	untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-	ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-	newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-	renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-	deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
-	bits=''
-	if [ "${renamed}" == "0" ]; then
-		bits=">${bits}"
-	fi
-	if [ "${ahead}" == "0" ]; then
-		bits="*${bits}"
-	fi
-	if [ "${newfile}" == "0" ]; then
-		bits="+${bits}"
-	fi
-	if [ "${untracked}" == "0" ]; then
-		bits="?${bits}"
-	fi
-	if [ "${deleted}" == "0" ]; then
-		bits="x${bits}"
-	fi
-	if [ "${dirty}" == "0" ]; then
-		bits="!${bits}"
-	fi
-	if [ ! "${bits}" == "" ]; then
-		echo "${bits}"
-	else
-		echo ""
-	fi
+	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+	if [ ! "${BRANCH}" == "" ]
+	then
+        status=`git status 2>&1 | tee`
+        dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+        untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+        ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+        newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+        renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+        deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+        bits=''
+        if [ "${renamed}" == "0" ]; then
+            bits=">${bits}"
+        fi
+        if [ "${ahead}" == "0" ]; then
+            bits="*${bits}"
+        fi
+        if [ "${newfile}" == "0" ]; then
+            bits="+${bits}"
+        fi
+        if [ "${untracked}" == "0" ]; then
+            bits="?${bits}"
+        fi
+        if [ "${deleted}" == "0" ]; then
+            bits="x${bits}"
+        fi
+        if [ "${dirty}" == "0" ]; then
+            bits="!${bits}"
+        fi
+        if [ ! "${bits}" == "" ]; then
+            echo "${bits}"
+        else
+            echo ""
+        fi
+    else
+        echo ""
+    fi
 }
 
-echo `parse_git_branch`
-export PS1="\[\e[31m\]\`nonzero_return\`\[\e[m\]\[\e[35m\][\t]\[\e[m\]\[\e[35m\]\u:\[\e[m\]\[\e[34m\]\W\[\e[m\]"`parse_git_branch`" \n$ "
+export PS1="\[\e[31m\]\`nonzero_return\`\[\e[m\]\[\e[35m\][\t]\[\e[m\]\[\e[35m\]\u:\[\e[m\]\[\e[34m\]\W\[\e[m\]\[\e[32m\][\`parse_git_branch\`->\[\e[m\]\[\e[31m\]\`parse_git_remote_branch\`|\[\e[m\]\[\e[32m\]\`parse_git_dirty\`]\[\e[m\] \n$ "
